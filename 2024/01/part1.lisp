a19a547b9141a0180b34640f38955e4e01c3a301
Joachim Le Fournis <joachimlf@redgl0w.net>
2024-12-02 07:35:48 +0100

    Day 1 of 2024

---
+++ 2024/01/part1.lisp
@@ -0,0 +1,37 @@
+#!/usr/bin/sbcl --script
+
+(defun split-input (s)
+  "We iter from start, and each time we see a space we go to 3 char after
+  (the column are separated in the input by 3 space chars)"
+  (loop for i = 0 then (+ j 3)
+    as j = (position #\Space s :start i)
+    collect (parse-integer (subseq s i j))
+    while j))
+
+(defun list-of-couple-to-couple-of-list (l)
+  (list
+    (mapcar #'car l)
+    (mapcar #'cadr l)
+  )
+)
+
+(defun parse-file (filename)
+  (with-open-file (stream filename)
+    (loop for line = (read-line stream nil)
+          while line
+          collect (split-input line))))
+
+(defun sum-difference (lists)
+  (apply '+ 
+    (loop for i in (car lists)
+        for j in (cadr lists)
+        collect (abs (- i j))
+    )
+  )
+)
+
+(defun sorted-input (filename)
+  (mapcar #'(lambda (l) (sort l #'<)) (list-of-couple-to-couple-of-list (parse-file filename)))
+)
+
+(write (sum-difference (sorted-input "input.txt")))
